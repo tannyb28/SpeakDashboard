63e440874bbf44568d9021f32cad1b6c42022681
Tanish Bhowmick <tanishbhowmick@gmail.com>
2025-02-20 02:22:23 -0500

    adjusted pdfjs worker

--- src/components/modals/ExerciseDetailModal.tsx
+++ src/components/modals/ExerciseDetailModal.tsx
@@ -7,10 +7,7 @@ import { pdfjs, Document, Page } from 'react-pdf';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/build/pdf.worker.min.mjs',
-  import.meta.url
-).toString();
+pdfjs.GlobalWorkerOptions.workerSrc = '/pdf.worker.min.mjs';
 
 const options = {
   cMapUrl: '/cmaps/',
